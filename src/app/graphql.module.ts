a63ce8b63c166a3733ad70a12aef21fe8f913f80
Damion Robinson <damion.robinson@hypi.io>
2019-05-02 13:15:01 +0100

    latest todo with grapql setup

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,23 +3,47 @@ import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloLink, concat } from 'apollo-link';
+import { RetryLink } from 'apollo-link-retry';
 
-const uri = 'https://api2.hypi.io/graphql'; // <-- add the URL of the GraphQL server here
+const HttpGraphqlURI = 'https://play.hypi.io/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink) {
+  const token = 'eyJhbGciOiJSUzI1NiIsInppcCI6IkRFRiJ9.eNqszE0OgyAQhuG7zFoaLX_qWdgMMEYTtEYgaWO8eyleodtnvnlPmD_78tizDUuc6YARTgMHYVgNjKZeDTQGNlypSkyvgyodFAjjrQETxVT5fhm3HMIFzd1ftphwc_T_PGZfomQ910oh863WTEhumZ0mYs471_NOiUn5Ml4wwdhJKWTftlI2QO-9gn7KXvwgZltyAqmMOs6GoZREP0hmNSIjr_AptNbd1ML1BQAA__8.P6OLUzh-8rn1K0aieYF34RS2zT-rgC-p9A5ebhzCK0n3LU8yRd-5BrkWwsRg_SLqwgIDfHSRbzjWY-o0PoMu5A6f1bgW84tdiQOfNl8pascgbwf5ZCT1jccp1WvWKPKFwMuAaFYbSU2ZCCo4DCs-RtUw74QMN328sx8g5ONOmTvTlyPwALYfyCLZUWgyT5rDSvsUGOp5eDbzaAqIVp19OcSrTYkWs7GZxrVMR6XtuJjyR3ieCHvTJ6ibxaaUYtK8CitUgsRBeGy-XRaTpfGbgw3KlbnR5wsNTOYlgwMqk2Hhlx30Y6mJYbrrhvBLLgW2NmlN4hxaXMQ9TPv952b6Jg';
+
+  const _retryLink = new RetryLink({
+    delay: {
+      initial: 1000,
+      max: Infinity,
+      jitter: true
+    },
+    attempts: {
+      max: 10,
+      retryIf: (error, _operation) => !!error
+    }
+  });
+
+  const _httpLink =  httpLink.create({
+    uri: HttpGraphqlURI,
+    withCredentials: true,
+  });
+
 
   const authMiddleware = new ApolloLink((operation, forward) => {
     operation.setContext({ // add the authorization to the headers
       headers: {
         'hypi-domain': 'latest.store.hypi.hypi.app',
-        // 'Authorization': auth_token ? `${auth_token}` : auth_token,
+        'Authorization': token,
       }
     });
     return forward(operation);
   });
 
+  const link = ApolloLink.from([
+    _retryLink,
+    _httpLink,
+  ]);
 
   return {
-    link: concat(authMiddleware, httpLink.create({uri})),
+    link: concat(authMiddleware, link),
     cache: new InMemoryCache(),
   };
 }
